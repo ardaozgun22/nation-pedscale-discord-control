448589763e280e678a6c341ca9f46868209282e1
ardaozgun22 <ardaozgun22@gmail.com>
2025-10-15 21:51:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3001;
 
 app.use(cors());
 app.use(express.json());
